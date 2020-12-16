c62f79cce0ce65493dc84169f2b93d5dfdd12995
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-16 23:32:22 +0100

    Initial version

--- TuneSearchAssignment1/ContentView.swift
+++ TuneSearchAssignment1/ContentView.swift
@@ -6,11 +6,34 @@
 //
 
 import SwiftUI
+import TunesearchMockCore
+import TunesearchCorePorts
 
 struct ContentView: View {
+    
+    let command = MockSearchTracksCommand()
+        
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear {
+                command.execute(inDTO: SearchTermDTO(term: "term")) {
+                    switch $0 {
+                    case let .success(collections):
+                        success(value: collections)
+                    case let .failure(error):
+                        failure(failure: error)
+                    }
+                }
+            }
+    }
+    
+    func failure(failure: Error) {
+        debugPrint(failure.localizedDescription)
+    }
+    
+    func success(value: [TrackCollection]) {
+        debugPrint(value)
     }
 }
 
